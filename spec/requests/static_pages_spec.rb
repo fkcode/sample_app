8bd207794f6348a403739fccb2d1f5a88500ddb7
fkcode <fkcode@gmx.com>
2013-06-10 14:35:50 +0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,49 +10,30 @@ describe PagesController do
     before { visit root_path }
 
     it { should have_selector("h1", :text => "Sample App") }
-
     it { should have_selector("title", :text => "#{base_title}") }
-
     it { should_not have_selector("title", :text => "| Home") }
   end
 
   describe "Contact page" do
-    it "should have some content" do
-      visit contact_path
-      page.should have_selector('h1', :text => "Contact")
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector("title", :text => "#{base_title} | Contact")
-    end
-
+    it { should have_selector('h1', :text => "Contact") }     
+    it { should have_selector("title", :text => "#{base_title} | Contact") }
   end
 
   describe "About page" do
-    it "should have some content" do
-      visit about_path
-      page.should have_selector('h1', :text => "About")
-    end
+    before { visit about_path }
 
-    it "should have the title 'About'" do
-      visit about_path
-      page.should have_selector("title", :text => "#{base_title} | About")
-    end
-
+    it { should have_selector('h1', :text => "About") }
+    it { should have_selector("title", :text => "#{base_title} | About")}
   end
 
   describe "Help page" do
-    it "should have some content" do
-      visit help_path
-      page.should have_selector('h1', :text => "Help")
-    end
+    before { visit help_path }
+    
+    it { should have_selector('h1', :text => "Help") }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector("title", :text => "#{base_title} | Help")
-    end
-
+    it { should have_selector("title", :text => "#{base_title} | Help") }
   end
 
 end
